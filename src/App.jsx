fad331432e9f74c7851bfc1a60ce34eb6595ec94
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-18 10:35:31 -0700

    Create 'SinglePokemon' component, imported and mounted to App.jsx; screen display 'SinglePokemon'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PokemonList from './components/PokemonList';
+import SinglePokemon from './components/SinglePokemon'
 
 function App() {
 
 
   return (
   
-    <PokemonList />
+    // <PokemonList />
+    <SinglePokemon />
 
   )
 }
---
+++ src/components/SinglePokemon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SinglePokemon(){
+    return (
+        <>
+        <h1>SinglePokemon</h1>
+        </>
+    )
+
+}
+
+export default SinglePokemon;
